d8b2b8fa49f9f882b860fd746a76cb8b10946239
Sarah <sarah.salam3679@gmail.com>
2023-08-15 00:23:08 +0300

    added support for dark mode

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// Function to apply dark mode styles
+function applyDarkModeStyles() {
+    document.body.style.backgroundColor = "#tce9e2";
+    document.body.style.color = "#ffffff";
+    // Add more style adjustments as needed
+  }
+  
+  // Function to apply light mode styles
+  function applyLightModeStyles() {
+    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.color = "#333333";
+    // Add more style adjustments as needed
+  }
+  
+  // Listen for changes in color scheme preference
+  const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  
+  // Function to handle changes in color scheme preference
+  function handleColorSchemeChange(e) {
+    if (e.matches) {
+      applyDarkModeStyles();
+    } else {
+      applyLightModeStyles();
+    }
+  }
+  
+  // Initial call to apply styles based on current color scheme preference
+  handleColorSchemeChange(darkModeMediaQuery);
+  
+  // Attach event listener to handle future changes
+  darkModeMediaQuery.addEventListener('change', handleColorSchemeChange);
